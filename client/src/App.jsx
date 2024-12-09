6cd3c955fc15d3765c458991b294b9a2ee37cafc
dung dz <dungdx@gmail.com>
2024-12-09 22:03:35 +0700

    crate homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Heaader from "./components/Heaader";
+import Carousel from "./components/Carousel";
+import Categories from "./components/Categories";
+import Card from "./feature/products/Card";
+import Product from "./feature/products/Product";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="containerCustom">
+      <Heaader></Heaader>
+      <Carousel></Carousel>
+      <Categories></Categories>
+      <Product></Product>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,41 @@
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Roboto", "san-serif"],
+      headings: ["Poppins", "san-serif"],
+    },
+    extend: {
+      screens: {
+        "800px": "800px",
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f97316",
+
+          secondary: "#f000b8",
+
+          accent: "#1dcdbc",
+
+          neutral: "#2e2e3a",
+
+          "base-100": "#ffffff",
+
+          info: "#3abff8",
+
+          success: "#36d399",
+
+          warning: "#fbbd23",
+
+          error: "#f87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
