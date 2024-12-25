1e6abd27a938eac2d9b70b3f2efa3227e35f4cbf
dung dz <dungdx@gmail.com>
2024-12-25 15:22:37 +0700

    config router 2

--- client/src/layouts/LayoutClient.jsx
+++ client/src/layouts/LayoutClient.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const LayoutClient = () => {
   return (
     <>
       <div className="containerCustom">
+        <Header></Header>
         <Outlet></Outlet>
+        <Footer></Footer>
       </div>
     </>
   );
