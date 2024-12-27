9d6b05f5b697f6a98d5a69618905f785af0bb82c
dung dz <dungdx@gmail.com>
2024-12-27 21:48:26 +0700

    thong bao khi dang ki thanh cong gui email

--- server/controllers/authControll.js
+++ server/controllers/authControll.js
@@ -2,21 +2,54 @@ const User = require("../models/usersModel");
 const jwt = require("jsonwebtoken");
 const CatchAsync = require("../utils/CatchAsync");
 const HandelError = require("../utils/Error");
+const novu = require("../utils/novu");
 
-exports.signup = CatchAsync(async (req, res) => {
+
+// Đăng ký người dùng (signup)
+exports.signup = CatchAsync(async (req, res,next) => {
   const { email, password, name } = req.body;
 
-  const user = await User.create({ email, password, name });
-  res.status(200).json({ success: true, message: "Signup successful!", user });
+  // Kiểm tra nếu người dùng đã tồn tại
+  let user = await User.findOne({ email });
+  if (user) {
+    return next(new HandelError("User already exists", 400));
+  }
+
+  // Tạo người dùng trực tiếp, bỏ qua bước active
+  user = await User.create({ email, password, name });
+
+  // Tạo subscriber trong Novu
+  await novu.subscribers.identify(user._id, {
+    firstName: name,
+    email,
+  });
+
+  // Gửi thông báo (nếu cần)
+  await novu.trigger("demo-recent-login", {
+    to: {
+      subscriberId: user._id, // Sử dụng ID của người dùng làm subscriberId
+    },
+    payload: {
+      userName: name,
+      companyName: "dungdq03",
+    },
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Signup successful!",
+    user,
+  });
 });
-exports.signin = CatchAsync(async (req, res,next) => {
+
+exports.signin = CatchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   const yesUser = await User.findOne({ email }).select("+password");
   console.log("User found:", yesUser);
 
   if (!yesUser) {
-   return next(new HandelError("khong tim thay user cua ban (email)",400))
+    return next(new HandelError("khong tim thay user cua ban (email)", 400));
   } else {
     const comparePass = await yesUser.comparePassword(
       password,
@@ -35,7 +68,10 @@ exports.signin = CatchAsync(async (req, res,next) => {
         token,
       });
     } else {
-      return next( new HandelError("mat khau khong dung vui long nhap lai"),400)
+      return next(
+        new HandelError("mat khau khong dung vui long nhap lai"),
+        400
+      );
     }
   }
 });
