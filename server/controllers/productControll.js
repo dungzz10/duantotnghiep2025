49c862167e8ac82ceda0ac3fb4428b3e4a51198b
hocclnh <hocntph22459@fpt.edu.vn>
2025-02-05 12:39:22 +0700

    back end crud categories , contactus chuyen sang viet theo es6

--- server/controllers/productControll.js
+++ server/controllers/productControll.js
@@ -1,9 +1,58 @@
-const Product = require("../models/productModel");
-const CatchAsync = require("../utils/CatchAsync");
-const HandelError = require("../utils/Error");
-const User = require("../models/usersModel");
+import Product from"../models/productModel.js";
+import CatchAsync from"../utils/CatchAsync.js";
+import HandelError from"../utils/Error.js";
+import User from"../models/usersModel.js";
+// list san pham
+export const GetallProduct = async (req, res) => {
+  try {
+      const product = await Product.find().populate("CategoryId")
+      if (product.length === 0) {
+          return res.status(404).json({
+              message: "Không có sản phẩm nào",
+          });
+      }
+      return res.status(200).json({
+          message: "thành công",
+          data: product
+      });
+  } catch (error) {
+      return res.status(500).json({
+          message: error.message,
+      });
+  }
+};
+
+// export const getOneProduct = async function (req, res) {
+//   const productId = req.params.productId; // Lấy id của bài đăng
+//   try {
+//       const product = await Product.findById(req.params.id).populate("tags").populate("CategoryId")
+//       if (!product) {
+//           return res.status(404).json({
+//               message: "Không tìm thấy sản phẩm",
+//           });
+//       }
+//       // Tìm các bài đăng liên quan
+//       const relatedProducts = await Product.find({
+//           $and: [
+//               { _id: { $ne: productId } },
+//               { $or: [{ tags: { $in: product.tags } }, { CategoryId: product.CategoryId }] }],
+//       }).limit(6).populate("tags");
+//       // Tăng số lượt xem lên một đơn vị
+//       product.views++;
+//       await product.save();
+//       return res.status(200).json({
+//           message: "thành công",
+//           data: product, relatedProducts
+//       });
+
+//   } catch (error) {
+//       return res.status(500).json({
+//           message: error.message,
+//       });
+//   }
+// };
 // tao san pham moi
-exports.createProduct = catchAsync(async (req, res, next) => {
+export const createProduct = CatchAsync(async (req, res, next) => {
   console.log(req.user)
   const productData = req.body;
   console.log(productData)
@@ -20,3 +69,16 @@ exports.createProduct = catchAsync(async (req, res, next) => {
   });
   //
 });
+export const RemoveProduct = async function (req, res) {
+  try {
+      const product = await Product.findByIdAndDelete(req.params.id);
+      return res.status(200).json({
+          message: "Xóa sản phẩm thành công",
+          product,
+      });
+  } catch (error) {
+      return res.status(500).json({
+          message: error.message,
+      });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const app = require("./app");
-const mongoose = require("mongoose");
+import app from "./app.js";
+import mongoose from "mongoose";
 
-const port = process.env.PORT || 5000;
-const mongoURL = process.env.MONGO_URL || "mongodb://localhost:27017/datn2025";
+const port =  5000;
+const mongoURL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/datn2025";
 // Xử lý các ngoại lệ chưa được bat
 process.on("uncaughtException", (err) => {
   console.log("Lỗi = ", +err.message); // In ra thông điệp loi
