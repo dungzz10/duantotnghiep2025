8135e5ca1c081a95d26add242d2c3af3db4bcdbb
dungzz10 <hoaquangdung24052003@gmail.com>
2025-02-12 18:23:19 +0700

    short delete khoi phuc san pham

--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, getAllProduct, getSingleProducts, updateProduct } from "../controllers/productControll.js";
+import { createProduct, getAllProduct, getSingleProducts, updateProduct,softDeleteProduct, restoreProduct } from "../controllers/productControll.js";
 import { isAuththenticated } from "../middlewares/auth.js";
 
 const productRouter = express.Router();
@@ -7,6 +7,8 @@ productRouter.post("/", isAuththenticated, createProduct);
 productRouter.get("/", getAllProduct);
 productRouter.get("/:id", isAuththenticated,getSingleProducts);   
 productRouter.put("/:id", isAuththenticated, updateProduct);
+productRouter.put("/delete/:id", isAuththenticated, softDeleteProduct);
+productRouter.put("/khoiphuc/:id", isAuththenticated,restoreProduct );
 // productRouter.post(":id", createProduct);
 
 export default productRouter
