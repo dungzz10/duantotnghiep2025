c863d7e5320caba7bc5d3c418d9c5bd250c6430e
Le Quang Sang <sanglq125@gmail.com>
2025-02-21 17:14:56 +0700

    update footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,30 +1,125 @@
-import React from 'react'
+import React from "react";
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram } from "react-icons/fa";
+import Wrapper from "./Wrapper";
 
 const Footer = () => {
-  return (
-    <footer className="footer section bg-neutral text-neutral-content p-10">
-  <nav>
-    <h6 className="footer-title">Dịch vụ</h6>
-    <a className="link link-hover">Xây dựng thương hiệu</a>
-    <a className="link link-hover">Thiết kế</a>
-    <a className="link link-hover">Tiếp thị</a>
-    <a className="link link-hover">Quảng cáo</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Công ty</h6>
-    <a className="link link-hover">Về chúng tôi</a>
-    <a className="link link-hover">Liên hệ</a>
-    <a className="link link-hover">Việc làm</a>
-    <a className="link link-hover">Bộ báo chí</a>
-  </nav>
-  <nav>
-    <h6 className="footer-title">Hợp pháp</h6>
-    <a className="link link-hover">Điều khoản sử dụng</a>
-    <a className="link link-hover">Chính sách bảo mật</a>
-    <a className="link link-hover">Chính sách đổi trả</a>
-  </nav>
-</footer>
-  )
-}
+    return (
+        <footer className="bg-black text-white pt-14 pb-3">
+            <Wrapper className="flex justify-between flex-col md:flex-row gap-[50px] md:gap-0">
+                {/* PHẦN TRÁI */}
+                <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] flex-col md:flex-row">
+                    {/* DANH MỤC */}
+                    <div className="flex flex-col gap-3 shrink-0">
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            Tìm cửa hàng
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            Trở thành đối tác
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            Đăng ký nhận email
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            Gửi phản hồi cho chúng tôi
+                        </div>
+                        <div className="font-oswald font-medium uppercase text-sm cursor-pointer">
+                            Giảm giá cho sinh viên
+                        </div>
+                    </div>
+                    {/* KẾT THÚC DANH MỤC */}
 
-export default Footer
+                    {/* DANH MỤC BÌNH THƯỜNG */}
+                    <div className="flex gap-[50px] md:gap-[75px] lg:gap-[100px] shrink-0">
+                        {/* DANH MỤC */}
+                        <div className="flex flex-col gap-3">
+                            <div className="font-oswald font-medium uppercase text-sm">
+                                Hỗ trợ
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Trạng thái đơn hàng
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Giao hàng
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Đổi trả
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Phương thức thanh toán
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Liên hệ chúng tôi
+                            </div>
+                        </div>
+                        {/* KẾT THÚC DANH MỤC */}
+
+                        {/* DANH MỤC */}
+                        <div className="flex flex-col gap-3">
+                            <div className="font-oswald font-medium uppercase text-sm">
+                                Về chúng tôi
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Tin tức
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Tuyển dụng
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Nhà đầu tư
+                            </div>
+                            <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
+                                Bền vững
+                            </div>
+                        </div>
+                        {/* KẾT THÚC DANH MỤC */}
+                    </div>
+                    {/* KẾT THÚC DANH MỤC BÌNH THƯỜNG */}
+                </div>
+                {/* KẾT THÚC PHẦN TRÁI */}
+
+                {/* PHẦN PHẢI */}
+                <div className="flex gap-4 justify-center md:justify-start">
+                    <div onClick={() => window.open('https://facebook.com')} className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                        <FaFacebookF size={20} />
+                    </div>
+                    <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                        <FaTwitter size={20} />
+                    </div>
+                    <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                        <FaYoutube size={20} />
+                    </div>
+                    <div className="w-10 h-10 rounded-full bg-white/[0.25] flex items-center justify-center text-black hover:bg-white/[0.5] cursor-pointer">
+                        <FaInstagram size={20} />
+                    </div>
+                </div>
+                {/* KẾT THÚC PHẦN PHẢI */}
+            </Wrapper>
+            <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
+                {/* BÊN TRÁI */}
+                <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
+                    © 2025 
+                </div>
+                {/* KẾT THÚC BÊN TRÁI */}
+
+                {/* BÊN PHẢI */}
+                <div className="flex gap-2 md:gap-5 text-center md:text-left flex-wrap justify-center">
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Hướng dẫn
+                    </div>
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Điều khoản bán hàng
+                    </div>
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Điều khoản sử dụng
+                    </div>
+                    <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer">
+                        Chính sách bảo mật
+                    </div>
+                </div>
+                {/* KẾT THÚC BÊN PHẢI */}
+            </Wrapper>
+        </footer>
+    );
+};
+
+export default Footer;
