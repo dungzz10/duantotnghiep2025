18633840079af3af73240429a91f3b35d6b2e2d7
Le Quang Sang <sanglq125@gmail.com>
2025-02-26 20:33:58 +0700

    hiển thị banner ra client

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+import { fetchBanners } from "../features/admin/banners/listBannerAdmin/apiListBanner";
 
 const Banner = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['products'],
+    queryFn: fetchBanners
+  })
+  
+  console.log(data);
+
+  if (isLoading) return <p className="text-center text-lg text-gray-600">Đang tải dữ liệu...</p>
+  if (error) return <p className="text-center text-lg text-red-500">Lỗi tải dữ liệu</p>
   return (
     <div className="relative text-[20px] w-full max-w-[1360px] mx-auto text-white">
       <Carousel
@@ -12,15 +24,13 @@ const Banner = () => {
         showIndicators={false}
         showStatus={false}
       >
-        <div>
-          <img src="/src/assets/slide-1.png" />
-        </div>
-        <div>
-          <img src="/src/assets/slide-2.png" />
-        </div>
-        <div>
-          <img src="/src/assets/slide-3.png" />
-        </div>
+        {data?.banners?.map((banner, index) => (
+          <div key={index}>
+            <img
+            className='w-full h-auto max-h-[500px] object-cover'
+            src={banner.image} alt={`Banner ${index + 1}`} />
+          </div>
+        ))}
       </Carousel>
     </div>
   );
