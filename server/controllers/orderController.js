00b3048c1a27fa0ac46b29dd0c5a64b2825aa86f
dung dz <dungdx@gmail.com>
2025-03-01 15:46:15 +0700

    them role superadmin vào getorrder

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -5,20 +5,21 @@ import mongoose from "mongoose";
 
 export const getAllOrders = CatchAsync(async (req, res, next) => {
   const { id: userId, role } = req.user;
+  console.log("id", userId);
 
   if (!userId) {
     return next(new HandelError("Người dùng chưa đăng nhập", 401));
   }
 
-  const filter = role === "admin" ? {} : { userId };
-  const orders = await Order.find(filter)
-  .populate("userId", "name")
-  .populate({
-    path: "products.productId",
-    select: "name price image",
-  })
-  .lean();
+  const filter = role === "admin" || role === "superadmin" ? {} : { userId };
 
+  const orders = await Order.find(filter)
+    .populate("userId", "name")
+    .populate({
+      path: "products.productId",
+      select: "name price image",
+    })
+    .lean();
 
   if (!orders || orders.length === 0) {
     return next(new HandelError("Không tìm thấy đơn hàng", 404));
@@ -30,9 +31,10 @@ export const getAllOrders = CatchAsync(async (req, res, next) => {
 export const getOrderById = CatchAsync(async (req, res, next) => {
   const { orderId } = req.params;
 
-
   if (!mongoose.Types.ObjectId.isValid(orderId)) {
-    return next(new HandelError(`Tham số không hợp lệ: orderId = ${orderId}`, 400));
+    return next(
+      new HandelError(`Tham số không hợp lệ: orderId = ${orderId}`, 400)
+    );
   }
 
   const order = await Order.findById(orderId)
@@ -52,7 +54,13 @@ export const getOrderById = CatchAsync(async (req, res, next) => {
 export const createCODOrder = CatchAsync(async (req, res, next) => {
   console.log("Nhận yêu cầu thanh toán COD:", req.body);
 
-  const { products, amount, shippingAddress, shippingFee = 0, voucherDiscount = 0 } = req.body;
+  const {
+    products,
+    amount,
+    shippingAddress,
+    shippingFee = 0,
+    voucherDiscount = 0,
+  } = req.body;
   const finalTotal = amount + shippingFee - voucherDiscount;
   if (!amount || isNaN(amount) || finalTotal < 1000) {
     return next(new HandelError("Số tiền không hợp lệ", 400));
@@ -71,69 +79,78 @@ export const createCODOrder = CatchAsync(async (req, res, next) => {
     }
   }
 
-    const orderId = `ORD-${Date.now()}`;
+  const orderId = `ORD-${Date.now()}`;
 
-    const newOrder = await Order.create({
-      userId,
-      orderId,
-      amount,
-      products,
-      shippingAddress,
-      shippingFee,
-      voucherDiscount,
-      finalTotal,
-      paymentMethod: "COD",
-      paymentStatus: "pending",
-      orderStatus: "pending",
-      date: new Date(),
-    });
-
-    console.log("Đơn hàng COD đã được lưu vào DB:", newOrder);
-
-    res.status(201).json({
-      success: true,
-      message: "Đơn hàng COD đã được tạo thành công",
-      order: newOrder,
-    });
+  const newOrder = await Order.create({
+    userId,
+    orderId,
+    amount,
+    products,
+    shippingAddress,
+    shippingFee,
+    voucherDiscount,
+    finalTotal,
+    paymentMethod: "COD",
+    paymentStatus: "pending",
+    orderStatus: "pending",
+    date: new Date(),
   });
 
-  export const updateOrder = CatchAsync(async (req, res, next) => {
-    const { orderId } = req.params;
-    const { orderStatus } = req.body;
-  
+  console.log("Đơn hàng COD đã được lưu vào DB:", newOrder);
 
-    const validStatuses = ["pending", "processing", "shipped", "delivered", "cancelled"];
-    if (orderStatus && !validStatuses.includes(orderStatus)) {
-        return next(new HandelError("Trạng thái đơn hàng không hợp lệ", 400));
-    }
-  
-    const order = await Order.findByIdAndUpdate(orderId, { orderStatus }, {
+  res.status(201).json({
+    success: true,
+    message: "Đơn hàng COD đã được tạo thành công",
+    order: newOrder,
+  });
+});
+
+export const updateOrder = CatchAsync(async (req, res, next) => {
+  const { orderId } = req.params;
+  const { orderStatus } = req.body;
+
+  const validStatuses = [
+    "pending",
+    "processing",
+    "shipped",
+    "delivered",
+    "cancelled",
+  ];
+  if (orderStatus && !validStatuses.includes(orderStatus)) {
+    return next(new HandelError("Trạng thái đơn hàng không hợp lệ", 400));
+  }
+
+  const order = await Order.findByIdAndUpdate(
+    orderId,
+    { orderStatus },
+    {
       new: true,
       runValidators: true,
-    });
-  
-    if (!order) {
-      return next(new HandelError("Không tìm thấy đơn hàng", 404));
     }
-  
-    res.status(200).json({
-      success: true,
-      message: "Đơn hàng đã được cập nhật thành công",
-      order,
-    });
+  );
+
+  if (!order) {
+    return next(new HandelError("Không tìm thấy đơn hàng", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Đơn hàng đã được cập nhật thành công",
+    order,
   });
-  
-  export const deleteOrder = CatchAsync(async (req, res, next) => {
-    const { orderId } = req.params;
-  
-    const order = await Order.findOneAndDelete({ orderId });
-  
-    if (!order) {
-      return next(new HandelError("Không tìm thấy đơn hàng", 404));
-    }
-  
-    res.status(200).json({
-      success: true,
-      message: "Đơn hàng đã được xoá thành công",
-    });
+});
+
+export const deleteOrder = CatchAsync(async (req, res, next) => {
+  const { orderId } = req.params;
+
+  const order = await Order.findOneAndDelete({ orderId });
+
+  if (!order) {
+    return next(new HandelError("Không tìm thấy đơn hàng", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Đơn hàng đã được xoá thành công",
   });
+});
