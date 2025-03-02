11684f3f6f5b65daac80fe7ddcaa1b4f461047af
huynhuyn18 <hvu027849@gmail.com>
2025-03-02 21:09:18 +0700

    be favourites

--- server/controllers/favouriteController.js
+++ server/controllers/favouriteController.js
@@ -1,8 +1,15 @@
+import productModel from "../models/productModel.js";
 import usersModel from "../models/usersModel.js";
 
 export const createFavourite = async (req, res) => {
   try {
-    const { userId, productId } = req.params;
+    console.log(12344, req.user);
+
+    const userId = req.user.id;
+    const productId = req.body.productId;
+
+    console.log(userId, productId, 99999);
+
     const user = await usersModel.findById(userId);
 
     if (!userId) {
@@ -25,9 +32,29 @@ export const createFavourite = async (req, res) => {
   }
 };
 
+export const getFavourites = async (req, res) => {
+  const userId = req.user.id;
+
+  const user = await usersModel.findById(userId);
+
+  if (!user) {
+    throw new Error(message, "tai khoan k ton tai");
+  }
+
+  res.status(201).json({
+    success: true,
+    data: {
+      favourites: user.favourites,
+    },
+  });
+};
+
 export const removeFavourite = async (req, res) => {
   try {
-    const { userId, productId } = req.params;
+    const userId = req.user.id;
+
+    const productId = req.body.productId;
+
     const user = await usersModel.findById(userId);
 
     if (!user) {
