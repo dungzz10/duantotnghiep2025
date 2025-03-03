7c19fcb4ce8fe2e718508f4522014a211acc63da
Le Quang Sang <sanglq125@gmail.com>
2025-03-03 14:57:30 +0700

    sửa product card

--- client/src/features/product/Products.jsx
+++ client/src/features/product/Products.jsx
@@ -12,15 +12,14 @@ const Products = () => {
   return (
     <div className="section px-2 800px:px-0">
       <h1 className="font-bold text-2xl my-6">Các sản phẩm có sẵn</h1>
-      <Row gutter={[16, 16]}>
-        {data.pages.map((page) =>
-          page.products.map((product) => (
-            <Col key={product._id} xs={24} sm={12} md={8} lg={6}>
-              <Card product={product} />
-            </Col>
-          ))
-        )}
-      </Row>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-14 px-5 md:px-0">
+  {data?.pages.flatMap((page) =>
+    page.products.map((product) => (
+      <Card key={product._id} product={product} />
+    ))
+  )}
+</div>
+
       {hasNextPage && (
         <Button
           onClick={() => fetchNextPage()} // Tải thêm sản phẩm khi nhấn
