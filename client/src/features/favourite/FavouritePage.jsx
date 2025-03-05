a34adc0dc8e1181baf4788fcea376df86a70653c
huynhuyn18 <hvu027849@gmail.com>
2025-03-05 21:23:59 +0700

    hiển thị

--- client/src/features/favourite/FavouritePage.jsx
+++ client/src/features/favourite/FavouritePage.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../../axios/api";
+
 const FavouritePage = () => {
-  const [product, setProducts] = useState([]);
+  const [product, setProducts] = useState({ favourites: [] });
 
   useEffect(() => {
     async function fetchData() {
       const response = await api.get("/favourite");
       console.log(response.data.data.favourites, 9999);
 
-      setProducts(response.data.data);
+      setProducts({ favourites: response.data.data.favourites });
     }
     fetchData();
   }, []);
@@ -17,31 +18,31 @@ const FavouritePage = () => {
     try {
       await api.delete(`/favourite/${productId}`);
 
-      setProducts((prevProducts) => ({
-        ...prevProducts,
-        favourites: prevProducts.favourites.filter(
+      setProducts((prevState) => ({
+        favourites: prevState.favourites.filter(
           (item) => item._id !== productId
         ),
       }));
     } catch (error) {
-      console.error("loi khi xoa khoi danh sach yeu thich", error);
+      console.error("Lỗi khi xóa sản phẩm khỏi danh sách yêu thích", error);
     }
   };
+
   return (
     <>
       <main>
         <section className="container max-w-screen-xl m-auto mt-16">
           <div className="flex justify-between items-center mb-4">
-            <h2 className="fomt-semibold text-[40px]">New Products</h2>
+            <h2 className="font-semibold text-[40px]">Danh sách yêu thích</h2>
             <a
               href="./shop.html"
               className="border border-solid border-yellow-500 px-4 py-2 font-semibold text-base text-yellow-500 "
             >
-              View all products
+              Xem tất cả sản phẩm
             </a>
           </div>
           <div className="grid grid-cols-4 gap-8">
-            {product.favourites &&
+            {product.favourites && product.favourites.length > 0 ? (
               product.favourites.map((item) => (
                 <div key={item._id}>
                   <div className="overflow-hidden">
@@ -61,53 +62,20 @@ const FavouritePage = () => {
                         ? item.salePrice.toLocaleString() + "đ"
                         : item.originalPrice.toLocaleString() + "đ"}
                     </p>
-                    <button className="border border-solid border-yellow-700 text-yellow-700 w-full font-semibold text-base py-2 hover:bg-yellow-700 hover:text-white">
-                      Add to cart
-                    </button>
+
                     <br />
                     <button
                       onClick={() => handleRemoveFavourite(item._id)}
                       className="mt-4 border border-solid border-red-700 text-red-700 w-full font-semibold text-base py-2 hover:bg-yellow-700 hover:text-white"
                     >
-                      XÓA KHỎI DANH SÁCH YÊU THÍCH
+                      Xóa khỏi danh sách yêu thích
                     </button>
                   </div>
                 </div>
-              ))}
-          </div>
-        </section>
-        <section className="bg-[#FFF7ED] py-16 mt-16">
-          <div className=" container max-w-screen-xl m-auto grid grid-cols-4">
-            <div className="flex gap-5">
-              <img src="./assets/images/quality.png" alt />
-              <div>
-                <h3 className="font-semibold text-xl">Chất lượng cao </h3>
-                <p className="text-[#898989]">
-                  Được chế tác từ những vật liệu hàng đầu
-                </p>
-              </div>
-            </div>
-            <div className="flex gap-5">
-              <img src="./assets/images/support.png" alt />
-              <div>
-                <h3 className="font-semibold text-xl">Hỗ trợ 24/7 </h3>
-                <p className="text-[#898989]">Hỗ trợ tận tình</p>
-              </div>
-            </div>
-            <div className="flex gap-5">
-              <img src="./assets/images/protection.png" alt />
-              <div>
-                <h3 className="font-semibold text-xl">Bảo hành </h3>
-                <p className="text-[#898989]">6 tháng </p>
-              </div>
-            </div>
-            <div className="flex gap-5">
-              <img src="./assets/images/shipping.png" alt />
-              <div>
-                <h3 className="font-semibold text-xl">Miễn phí ship </h3>
-                <p className="text-[#898989]">Các đơn từ 300k</p>
-              </div>
-            </div>
+              ))
+            ) : (
+              <p>Danh sách yêu thích trống.</p>
+            )}
           </div>
         </section>
       </main>
