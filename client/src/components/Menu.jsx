24071dfbe54e5482df92ed417670463e14569e35
hocclnh <hocntph22459@fpt.edu.vn>
2025-03-05 19:58:23 +0700

    lam trang danh muc

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,61 +1,54 @@
-import React from 'react'
-import { BsChevronDown } from 'react-icons/bs'
+import React from 'react';
+import { BsChevronDown } from 'react-icons/bs';
 import { Link } from "react-router-dom";
+import useFetchData from '../app/api/useFetchdata';
 
 const Menu = ({ showCatMenu, setShowCatMenu }) => {
-    const data = [
+    const { data: categories } = useFetchData('categories');
+
+    const menu = [
         { id: 1, name: "Trang chủ", url: "/" },
-        { id: 2, name: "Giới thiệu ", url: "/about" },
-        { id: 3, name: "Danh mục ", url: "/categories", subMenu: true },
-        { id: 4, name: "Liên hệ", url: "/contact" },
+        { id: 1, name: "Sản phẩm", url: "/san-pham" },
+        { id: 2, name: "Giới thiệu", url: "/gioi-thieu" },
+        { id: 3, name: "Danh mục", subMenu: true },
+        { id: 4, name: "Liên hệ", url: "/lien-he" },
     ];
 
-    const subMenuData = [
-        { id: 3.1, name: "Sản phẩm", url: "/products" },
-        { id: 3.2, name: "Phụ kiện", url: "/accessory" },
-        { id: 3.3, name: "Giảm giá", url: "/product/sale" },
-        { id: 3.4, name: "Mới ra mắt", url: "/product/new" },
-        { id: 3.5, name: "Thương hiệu", url: "/branch" },
-    ];
     return (
         <ul className='hidden md:flex items-center gap-8 font-medium text-black'>
-            {data.map((item) => {
-                return (
-                    <React.Fragment key={item.id}>
-                        {!!item.subMenu ?
-                            <li className='cursor-pointer flex items-center gap-2 relative'
-                                onMouseEnter={() => setShowCatMenu(true)}
-                                onMouseLeave={() => setShowCatMenu(false)}
-                            >
-                                {item.name}
-                                <BsChevronDown size={14} />
-                                {showCatMenu && (
-                                    <ul
-                                        className='bg-white absolute top-6 left-0 min-w-[250px] 
-                            px-1 py-1 text-black shadow-lg'>
-                                        {subMenuData.map((submenu) => {
-                                            return (
-                                                <li className='h-12 flex justify-between items-center
-                                         px-3 hover:bg-black/[0.05] rounded-md'>
-                                                    <Link to={submenu?.url} className="flex-1">{submenu.name}</Link>
-                                                    <span className='opacity-50 text-sm'>99</span>
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
-                                )}
-                            </li>
-                            :
-                            (<li className='cursor-pointer'>
-                                <Link to={item?.url}>{item.name}</Link>
-                            </li>)
-                        }
-                    </React.Fragment>
-                )
-            })}
-
+            {menu.map((item) => (
+                <React.Fragment key={item.id}>
+                    {item.subMenu ? (
+                        <li className='cursor-pointer flex items-center gap-2 relative'
+                            onMouseEnter={() => setShowCatMenu(true)}
+                            onMouseLeave={() => setShowCatMenu(false)}
+                        >
+                            {item.name}
+                            <BsChevronDown size={14} />
+                            {showCatMenu && (
+                                <ul className='bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1 text-black shadow-lg'>
+                                    {categories && categories.length > 0 ? (
+                                        categories.map((category) => (
+                                            <li key={category.id} className='h-12 flex justify-between items-center px-3 hover:bg-black/[0.05] rounded-md'>
+                                                <Link to={`/danh-muc/${category._id}`} className="flex-1">{category.name}</Link>
+                                                <span className='opacity-50 text-sm'>{category.products.length}</span>
+                                            </li>
+                                        ))
+                                    ) : (
+                                        <li className='h-12 flex justify-center items-center px-3'>Loading...</li>
+                                    )}
+                                </ul>
+                            )}
+                        </li>
+                    ) : (
+                        <li className='cursor-pointer'>
+                            <Link to={item.url}>{item.name}</Link>
+                        </li>
+                    )}
+                </React.Fragment>
+            ))}
         </ul>
-    )
+    );
 }
 
-export default Menu
+export default Menu;
