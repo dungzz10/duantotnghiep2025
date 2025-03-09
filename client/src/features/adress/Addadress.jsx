fff51f3b48f10ea318a594c0d718c713f5855920
dungzz10 <hoaquangdung24052003@gmail.com>
2025-03-09 18:35:41 +0700

    tim kiem

--- client/src/features/adress/Addadress.jsx
+++ client/src/features/adress/Addadress.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import AddAdddress from './AddAdddress';
+import { getAddress } from './useAddresApi';
 
 const Addadress = () => {
+  const {data,isLoading} = getAddress()
+  console.log(data)
   const openModal = () => {
     const modal = document.getElementById("adress_model");
     if (modal) {
       modal.showModal(); 
     }
   };
+  if(isLoading) return <p> loading ....</p>
 
   return (
     <div className='section'>
