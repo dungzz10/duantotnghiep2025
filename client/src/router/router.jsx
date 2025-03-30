15539c04254254fd6bf7c3df29d1d6bec4831cde
hocclnh <hocntph22459@fpt.edu.vn>
2025-03-30 13:53:50 +0700

    làm lại giao diện đăng ký đăng nhập

--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -50,6 +50,7 @@ import SingelProduct from "../features/productdetail/SingelProduct";
 import CategoriesPage from "../features/categories/CategoriesPage";
 import ContactAdmin from "../features/admin/contact/ContactAdmin";
 import Addadress from "../features/adress/Addadress";
+import SignupPage from "../features/signup/SignupPage";
 
 export const router = createBrowserRouter([
   {
@@ -154,7 +155,7 @@ export const router = createBrowserRouter([
   },
   {
     path: "/signup",
-    element: <Register />,
+    element: <SignupPage />,
   },
   {
     path: "/profile",
