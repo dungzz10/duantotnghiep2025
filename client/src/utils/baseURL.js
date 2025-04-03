8d6b0470ccb24be15a05c5b8d06bd36a55d78706
Le Quang Sang <sanglq125@gmail.com>
2025-04-03 18:47:25 +0700

    cho phép đường dẫn vercel

--- client/src/utils/baseURL.js
+++ client/src/utils/baseURL.js
@@ -2,6 +2,6 @@ export const getBaseUrl = () => {
     if (process.env.NODE_ENV === 'development') {
         return "http://localhost:5000"; // URL cho môi trường phát triển
     } else {
-        return "https://webbanhang-delta-green.vercel.app"; // URL cho môi trường sản xuất
+        return "https://duantotnghiep2025.vercel.app"; // URL cho môi trường sản xuất
     }
 }
